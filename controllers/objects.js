0e69b99958fd731e50960333b4970a8c75b9bed0
chadvidovcich <chad.vidovcich@gmail.com>
2022-06-09 16:48:24 -0700

    remove features

--- controllers/objects.js
+++ controllers/objects.js
@@ -11,92 +11,92 @@ const createAccountLimiter = rateLimit({
 
 module.exports = function(app) {
     // DISPLAY
-    app.get("/", (req, res) => {
-        var currentUser = req.user;
-        console.log(currentUser)
-        Character.aggregate([{ $sample: { size: 6 } }])
-        .then(character => {
-            res.render("index", { currentUser, character });
-        })
-        .catch(err => {
-            console.log(err.message);
-        });
-    });
+    // app.get("/", (req, res) => {
+    //     var currentUser = req.user;
+    //     console.log(currentUser)
+    //     Character.aggregate([{ $sample: { size: 6 } }])
+    //     .then(character => {
+    //         res.render("index", { currentUser, character });
+    //     })
+    //     .catch(err => {
+    //         console.log(err.message);
+    //     });
+    // });
 
-    app.get("/about", (req, res) => {
-        var currentUser = req.user;
-        res.render("about", { currentUser });
-    });
+    // app.get("/about", (req, res) => {
+    //     var currentUser = req.user;
+    //     res.render("about", { currentUser });
+    // });
 
-    app.get("/documentation", (req, res) => {
-        var currentUser = req.user;
-        res.render("documentation", { currentUser });
-    });
+    // app.get("/documentation", (req, res) => {
+    //     var currentUser = req.user;
+    //     res.render("documentation", { currentUser });
+    // });
 
     // Submit Form
-    app.get("/submit/new", (req, res) => {
-        if (req.user) {
-            var currentUser = req.user;
+    // app.get("/submit/new", (req, res) => {
+    //     if (req.user) {
+    //         var currentUser = req.user;
 
-            Planet.find()
-            .then(planet => {
-                res.render("submit", { planet, currentUser });
-            })
-            .catch(err => {
-                console.log(err.message);
-            });
-        } else {
-            res.redirect(`/login`); // UNAUTHORIZED
-        }
-    });
+    //         Planet.find()
+    //         .then(planet => {
+    //             res.render("submit", { planet, currentUser });
+    //         })
+    //         .catch(err => {
+    //             console.log(err.message);
+    //         });
+    //     } else {
+    //         res.redirect(`/login`); // UNAUTHORIZED
+    //     }
+    // });
 
     // CREATE PLANET
-    app.get("/planet/:name", function(req, res) {
-        if (req.user && req.user.username == "admin") {
-            // INSTANTIATE INSTANCE OF MODEL
-            const planet = new Planet();
-            console.log(req.params.name)
-            planet.name = (req.params.name).toString().replace(/ /g,"_")
-            planet.url = "/api/planet/" + planet.name
-            planet.image = "../images/" + planet.name + ".jpg"
+    // app.get("/planet/:name", function(req, res) {
+    //     if (req.user && req.user.username == "admin") {
+    //         // INSTANTIATE INSTANCE OF MODEL
+    //         const planet = new Planet();
+    //         console.log(req.params.name)
+    //         planet.name = (req.params.name).toString().replace(/ /g,"_")
+    //         planet.url = "/api/planet/" + planet.name
+    //         planet.image = "../images/" + planet.name + ".jpg"
 
-            console.log(planet)
+    //         console.log(planet)
 
-            // SAVE INSTANCE OF PLANET MODEL TO DB
-            planet.save()
-            .then( character => {
-                res.redirect(`/`);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        } else {
-            res.redirect(`/`);
-        }
-    });
+    //         // SAVE INSTANCE OF PLANET MODEL TO DB
+    //         planet.save()
+    //         .then( character => {
+    //             res.redirect(`/`);
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         });
+    //     } else {
+    //         res.redirect(`/`);
+    //     }
+    // });
 
     // CREATE CHARACTER
-    app.post("/character", createAccountLimiter, function(req, res) {
-        if (req.user) {
-            // INSTANTIATE INSTANCE OF MODEL
-            const character = new Character(req.body);
-            character.name = character.name.replace(/ /g,"_").toLowerCase()
-            character.url = "/api/character/" + character.name
-            if (!req.body.image) {
-                character.image = "../images/" + character.name + ".jpg"
-            }
-            // SAVE INSTANCE OF CHARACTER MODEL TO DB
-            character.save()
-            Planet.findOneAndUpdate({ name: character.originPlanet },
-                { $push: { residents: character.name } })
-            .then( character => {
-                res.redirect(`/`);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-        } else {
-            res.redirect(`/`);
-        }
-    });
+    // app.post("/character", createAccountLimiter, function(req, res) {
+    //     if (req.user) {
+    //         // INSTANTIATE INSTANCE OF MODEL
+    //         const character = new Character(req.body);
+    //         character.name = character.name.replace(/ /g,"_").toLowerCase()
+    //         character.url = "/api/character/" + character.name
+    //         if (!req.body.image) {
+    //             character.image = "../images/" + character.name + ".jpg"
+    //         }
+    //         // SAVE INSTANCE OF CHARACTER MODEL TO DB
+    //         character.save()
+    //         Planet.findOneAndUpdate({ name: character.originPlanet },
+    //             { $push: { residents: character.name } })
+    //         .then( character => {
+    //             res.redirect(`/`);
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //         });
+    //     } else {
+    //         res.redirect(`/`);
+    //     }
+    // });
 }
--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 8000;
 // const { body, validationResult } = require('express-validator');
 
 const bodyParser = require('body-parser');
-const rateLimit = require("express-rate-limit");
+// const rateLimit = require("express-rate-limit");
 
 app.enable("trust proxy");
 app.use(express.static('static'))
 
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10
-});
+// const apiLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 10
+// });
 
 // app.use("/api/", apiLimiter);
 
@@ -27,18 +27,18 @@ const apiLimiter = rateLimit({
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 
-var checkAuth = (req, res, next) => {
-    console.log("Checking authentication");
-    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
-        req.user = null;
-    } else {
-        var token = req.cookies.nToken;
-        var decodedToken = jwt.decode(token, { complete: true }) || {};
-        req.user = decodedToken.payload;
-    }
+// var checkAuth = (req, res, next) => {
+//     console.log("Checking authentication");
+//     if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+//         req.user = null;
+//     } else {
+//         var token = req.cookies.nToken;
+//         var decodedToken = jwt.decode(token, { complete: true }) || {};
+//         req.user = decodedToken.payload;
+//     }
 
-    next();
-};
+//     next();
+// };
 
 // app.use(checkAuth);
 
