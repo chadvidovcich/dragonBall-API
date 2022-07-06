873e4428663741b9a1bef8b425781d99260fff4d
chadvidovcich <chad.vidovcich@gmail.com>
2022-07-05 18:02:53 -0700

    add testing packages

--- server/server.js
+++ server/server.js
@@ -1,28 +1,4 @@
-// env variable
-require('dotenv').config({ path: './config.env' });
-
-// import express and configure
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-// use cors
-const cors = require('cors');
-app.use(cors());
-
-// import routes
-const mainRoutes = require('./routes/main.route');
-const characterRoutes = require('./routes/character.route');
-
-//database connection
-const dbo = require('./db/conn');
-dbo.connectToServer(function (err) {
-  if (err) console.error(err);
-});
-
-//routes
-app.use('/', mainRoutes);
-app.use('/api/character', characterRoutes);
+const app = require('./app');
 
 //start server
 const port = process.env.PORT || 8000;
