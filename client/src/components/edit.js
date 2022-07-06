fbe6ab42788d2316b6160b54ef172d551527cd81
chadvidovcich <chad.vidovcich@gmail.com>
2022-07-06 16:16:13 -0700

    change POST to PUT in update

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -48,7 +48,7 @@ export default function Edit() {
 
     // This will send a post request to update the data in the database.
     await fetch(`http://localhost:8000/api/character/update/${params.id}`, {
-      method: 'POST',
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
